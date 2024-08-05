f6cef0e4ced69eab5de5aac49dd22e8a57b61049
alex-Sandro-alves-de-lima <lexjustin5@gmail.com>
2024-08-05 16:59:11 -0300

    commit routes

---
+++ src/routes/routes.tsx
@@ -0,0 +1,42 @@
+
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import PrivateRoute from './privateRoutes';
+// import Error404 from '../components/site/error404';
+// import PrivateRoute from './privateRoutes';
+// import LoginSection from '../components/site/LoginSection';
+// //import CarouselSection from '../components/site/CarouselSection';
+// //import Site from '../components/site/Site';
+// import AppManut from '../components/manut/app';
+// import FeedbackSectionManut from '../components/manut/FeedbackSection';
+// import FeedbackSection from '../components/site/FeedbackSection';
+// import Site from '../components/site/Site';
+// import FuncionalidadesForm from '../components/manut/FuncionalidadesForm';
+// import ServicoForm from '../components/manut/ServicoForm';
+// import CarrosselForm from '../components/manut/CarrosselForm';
+
+
+
+const router = createBrowserRouter([
+  {path: "/",    element: (<App />),//Elemento principal
+   // errorElement: <Error404 />,
+    //Rotas privadas
+    children: [
+      //Principal
+    //  {        path: "FeedbackSectionManut", element: (<PrivateRoute tela='/FeedbackSectionManut' ><FeedbackSectionManut /></PrivateRoute>),},
+      {        path: "FeedbackSectionManut", element: (<PrivateRoute tela='/FeedbackSectionManut' ><App /></PrivateRoute>),},
+      // {        path: "CarrosselForm", element: (<PrivateRoute tela='/CarrosselForm' ><CarrosselForm /></PrivateRoute>),},
+      // {        path: "ServicoForm", element: (<PrivateRoute tela='/ServicoForm' ><ServicoForm /></PrivateRoute>),},
+      // {        path: "FuncionalidadesForm", element: (<PrivateRoute tela='/FuncionalidadesForm' ><FuncionalidadesForm /></PrivateRoute>),},
+      // {        path: "FeedbackSection", element: (<PrivateRoute tela='/FeedbackSection' ><FeedbackSection /></PrivateRoute>),},
+      // {        path: "site", element: (<PrivateRoute tela='/site' ><Site /></PrivateRoute>),},
+         
+    ],
+    }, //Rota publica
+    { path: 'login', element: <App /> },
+    { path: "*",    element: <App />,}
+]);
+
+
+
+export default router;
