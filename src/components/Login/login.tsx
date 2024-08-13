77ec897bbb14203ba12fd8707d6570d2f159ed07
alex-Sandro-alves-de-lima <lexjustin5@gmail.com>
2024-08-13 08:08:05 -0300

    ajustando logo da tela login

--- src/components/Login/login.tsx
+++ src/components/Login/login.tsx
@@ -32,7 +32,8 @@ const Login: React.FC = () => {
                 <Col xs={24} sm={24} md={16} lg={12} className="login-image" />
                 <Col xs={24} sm={24} md={8} lg={6} className="login-form-container">
                     <img
-                        src="./src/assets/logoaR.png" 
+                        src={'./src/assets/logoaR.png'} 
+          
                         alt="Logo"
                         className="login-logo"
                     />
